91488a24ac3a966db440a8540a2deaf1f9082b50
Daniel Dinnyes <ddinnyes@gmail.com>
2021-02-22 14:56:55 +0000

    added procedures for configuring newly bootstrapped system

--- install.scm
+++ install.scm
@@ -9,11 +9,121 @@ exec guile -e main -s "$0" "$@"
 (use-modules
  ((common utils) #:prefix utils:)
  ((common deps) #:prefix deps:)
+ ((ice-9 readline) #:select (readline))
  ((ice-9 regex) #:prefix regex:)
  ((ice-9 rdelim) #:prefix rdelim:)
  ((ice-9 popen) #:prefix popen:)
  ((ice-9 pretty-print) #:prefix pp:)
  ((srfi srfi-1) #:prefix srfi1:))
+
+;;; CONFIGURE
+
+(define (init-apt)
+  (let ((aptconf-dir (utils:path "" "etc" "apt" "apt.conf.d")))
+    (with-output-to-file (utils:path aptconf-dir "norecommends")
+      (lambda ()
+	(utils:println "APT::Get::Install-Recommends \"false\";")
+	(utils:println "APT::Get::Install-Suggests \"false\";"))))
+  (system* "apt" "update")
+  (system* "apt" "full-upgrade" "-y"))
+
+(define (init-network)
+  (let ((interfaces-dir (utils:path "" "etc" "network" "interfaces.d")))
+    (with-output-to-file (utils:path interfaces-dir "lo")
+      (lambda ()
+	(utils:println "auto" "lo")
+	(utils:println "iface" "lo" "inet" "loopback")))
+    (map
+     (lambda (dev)
+       (with-output-to-file (utils:path interfaces-dir dev)
+	 (lambda ()
+	   (utils:println "auto" dev)
+	   (utils:println "iface" dev "inet" "dhcp"))))
+     (filter
+      (lambda (dev) (regex:string-match "en[a-z0-9]+" dev))
+      (string-split (utils:system->string* "ls" (utils:path "" "sys" "class" "net")) #\newline)))))
+
+(define (configure-locale locale)
+  (system* "apt" "install" "-y" "locales")
+  (let* ((locales-file (utils:path "" "etc" "locale.gen"))
+	 (bak-file (string-append locales-file ".bak")))
+    (when (not (file-exists? bak-file))
+      (copy-file locales-file bak-file))
+    (when (file-exists? bak-file)
+      (call-with-input-file bak-file
+	(lambda (input-port)
+	  (let* ((content (rdelim:read-string input-port))
+		 (pattern (string-append "# (" locale " .*)$")))
+	    (call-with-output-file locales-file
+	      (lambda (output-port)
+		(regex:regexp-substitute/global
+		 output-port pattern content
+		 'pre 1 'post))))))
+      (delete-file bak-file)))
+  (system "locale-gen"))
+
+(define (configure-timezone timezone)
+  (setenv "DEBIAN_FRONTEND" "noninteractive")
+  (system* "apt" "install" "-y" "tzdata")
+  (unsetenv "DEBIAN_FRONTEND")
+  (utils:println "Configuring timezone:" timezone "...")
+  (let ((zone-file (utils:path "" "usr" "share" "zoneinfo" timezone))
+	(localtime-file (utils:path "" "etc" "localtime")))
+    (cond
+     ((file-exists? zone-file)
+      (when (file-exists? localtime-file)
+	(delete-file localtime-file))
+      (symlink zone-file localtime-file)
+      (system* "dpkg-reconfigure" "-f" "noninteractive" "tzdata"))
+     (else
+      (error "Zonefile does not exist!" zone-file)))))
+
+(define* (configure-keyboard #:key layout variant model options)
+  (setenv "DEBIAN_FRONTEND" "noninteractive")
+  (system* "apt" "install" "-y" "console-setup")
+  (unsetenv "DEBIAN_FRONTEND")
+  (with-output-to-file (utils:path "" "etc" "default" "keyboard")
+    (lambda ()
+      (display (string-append "XKBLAYOUT=" layout))
+      (newline)
+      (display (string-append "XKBVARIANT=" variant))
+      (newline)
+      (display (string-append "XKBOPTIONS=" options))
+      (newline)
+      (display (string-append "XKBMODEL=" model))
+      (newline)
+      (display "BACKSPACE=guess")
+      (newline)))
+  (system "setupcon")
+  (utils:println "Verifying keyboard layout...")
+  (let ((resp (readline "Please type \"Hello#123\" here: ")))
+    (when (not (string= resp "Hello#123"))
+      (utils:println "CHECK FAILED!!!")
+      (utils:println "Falling back to manual keyboard configuration...")
+      (system* "dpkg-reconfigure" "keyboard-configuration"))))
+
+(define (read-sudouser)
+  (utils:println "You can disable root user account by creating a sudo user instead.")
+  (readline "Type a name for sudo user (or leave it empty to keep the root account enabled): "))
+
+(define (init-sudouser sudouser)
+  (let ((username (or sudouser (read-sudouser))))
+    (cond
+     ((not (string-null? username))
+      (system* "apt" "install" "-y" "sudo")
+      (system* "useradd" "-m" "-G" "sudo" username "-s" "/bin/bash")
+      (utils:println "Set password for sudo user" username "!")
+      (while (not (zero? (system* "passwd" username)))
+	(utils:println "Passwords don't match! Please try again!"))
+      (system* "passwd" "-l" "root")
+      (system* "usermod" "-s" "/sbin/nologin" "root"))
+     (else
+      (utils:println "Set password for root user!")
+      (while (not (zero? (system "passwd")))
+	(utils:println "Passwords don't match! Please try again!"))))))
+
+
+;; BOOTSTRAP
 
 (define bootstrap-include-list
   (list "lsb-release" "info" "xz-utils" "dirmngr" "ca-certificates"))
