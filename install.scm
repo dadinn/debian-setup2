8c514f5c3af3a2d86aad6b7cbf2f945893d91ecc
Daniel Dinnyes <ddinnyes@gmail.com>
2021-02-25 14:14:42 +0000

    updated usage string

--- install.scm
+++ install.scm
@@ -241,9 +241,7 @@ exec guile -e main -s "$0" "$@"
 
 " (basename (car args)) " [OPTION...]
 
-Bootstraps Debian in target directory, then chroots into it and executes COMMAND in the freshly bootstrapped Debian environment.
-
-Also places configuration script debconf.sh in the target directory to help with automating the configuration.
+Bootstraps Debian in the target directory, and then configures the new system under a chroot environment.
 
 Valid options are:
 "))
