946e8f469adcfe258935eec73fa3be945cd3cada
Daniel Dinnyes <dadinn@besenczy.net>
2022-01-09 17:06:35 +0000

    added optional no-check-gpg? argument to bootstrap procedure

--- install.scm
+++ install.scm
@@ -246,14 +246,16 @@ exec guile -e main -s "$0" "$@"
 (define bootstrap-include-list
   (list "lsb-release" "info" "xz-utils" "dirmngr" "ca-certificates"))
 
-(define (bootstrap target arch release mirror)
+(define* (bootstrap target arch release mirror #:optional no-check-gpg?)
   (system* "apt" "install" "-y" "debootstrap")
   (utils:println "Bootstrapping Debian release" release "for archictecture" arch "...")
-  (when (not (zero?
-	(system* "debootstrap" "--arch" arch "--include"
-		 (string-join bootstrap-include-list ",")
-		 release target mirror)))
-    (error "Failed to bootstrap the Debian system!" target arch release mirror)))
+  (let ((command
+	 `("debootstrap" "--arch" ,arch "--include"
+	   ,(string-join bootstrap-include-list ",")
+	   ,@(if no-check-gpg? (list "--no-check-gpg") '())
+	   ,release ,target ,mirror)))
+    (when (not (zero? (apply system* command)))
+      (error "Failed to bootstrap the Debian system!" target arch release mirror))))
 
 ;;; MAIN FUNCTION
 
